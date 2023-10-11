a78173a62269747d51b350236ce0c45b3f1efa37
mrnaveira <47919901+mrnaveira@users.noreply.github.com>
2023-10-11 18:36:07 +0200

    remove wallet types

--- packages/snap/src/types.ts
+++ packages/snap/src/types.ts
@@ -1,19 +1,3 @@
-export type SetWalletParams = {
-    tari_wallet_daemon_url: string;
-};
-
-export type TariPermission = Object;
-
-export type SendWalletRequestParams = {
-    token: string,
-    walletRequest: WalletRequest
-};
-
-export type WalletRequest = {
-    method: string,
-    params: Object
-};
-
 export type TransferRequest = {
     amount: number,
     resource_address: string,
